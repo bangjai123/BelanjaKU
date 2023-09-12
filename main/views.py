884ecd3b9e5a545cbbdf167bae45f0973073ed0b
bangjai123 <znaufalilmi@gmail.com>
2023-09-12 10:57:13 +0700

    changing the app

--- main/views.py
+++ main/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'nama': 'Sebuah barang',
-        'jumlah': '7 buah',
-        'harga': 'Rp30.000,00',
-        'total' : 'Rp210.000,00'
+        'name': 'Name',
+        'amount': 'Amount',
+        'price': 'Price/Item',
+        'descripstion' : 'Descriptions'
     }
 
     return render(request, "main.html", context)
