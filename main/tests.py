fd7976b0945cbdd1d3f502b27f33f855707a91c7
bangjai123 <znaufalilmi@gmail.com>
2023-09-13 11:27:56 +0700

    menambahkan test

--- main/tests.py
+++ main/tests.py
@@ -10,3 +10,16 @@ class mainTest(TestCase):
     def test_main_using_main_template(self):
         response = Client().get('/main/')
         self.assertTemplateUsed(response, 'main.html')
+
+    def test_nama_siswa_benar(self):
+        response = response = Client().get('/main/')
+        self.assertContains(response,'Zaidan Naufal Ilmi')
+    
+    def test_npm_siswa_benar(self):
+        response = response = Client().get('/main/')
+        self.assertContains(response,'2206081761')
+
+    def test_kelas_siswa_benar(self):
+        response = response = Client().get('/main/')
+        self.assertContains(response,'PBP F')
+    
