a0ae612bcbdf9cc6a3996c7a531e42549602dcb4
bangjai123 <znaufalilmi@gmail.com>
2023-09-19 19:22:22 +0700

    tugas 3

--- main/models.py
+++ main/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Item(models.Model):
     name = models.CharField(max_length=255)
+    price = models.IntegerField()
     date_added = models.DateField(auto_now_add=True)
     amount = models.IntegerField()
     description = models.TextField()
