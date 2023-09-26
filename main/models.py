80f2bfbdfb7518436f6f8cd59aa882cf35cef181
bangjai123 <znaufalilmi@gmail.com>
2023-09-27 00:23:22 +0700

    Selesai sementara

--- main/models.py
+++ main/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 class Item(models.Model):
     name = models.CharField(max_length=255)
@@ -6,3 +8,4 @@ class Item(models.Model):
     date_added = models.DateField(auto_now_add=True)
     amount = models.IntegerField()
     description = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from main.views import show_main, create_item, show_xml
-from main.views import show_main, create_item, show_json, show_xml, show_json_by_id,show_xml_by_id
+from main.views import show_main, create_item, show_json, show_xml, show_json_by_id,show_xml_by_id,register,login_user,logout_user
+
+
 app_name = 'main'
 
 urlpatterns = [
@@ -10,4 +11,7 @@ urlpatterns = [
     path('xml/<int:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<int:id>/', show_json_by_id, name='show_json_by_id'), 
     path('json/', show_json, name='show_json'), 
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
