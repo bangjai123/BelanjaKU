e74cb13c2d9013ef5250c11813d686d607e31a89
bangjai123 <znaufalilmi@gmail.com>
2023-10-13 11:35:02 +0700

    selesai

--- main/urls.py
+++ main/urls.py
@@ -17,5 +17,5 @@ urlpatterns = [
     path('login/', login_user, name='login'),
     path('logout/', logout_user, name='logout'),
     path('get-product/', get_product_json, name='get_product_json'),
-    path('create-ajax/', add_product_ajax, name='add_product_ajax')
+    path('create-product-ajax/', add_product_ajax, name='add_product_ajax')
 ]
